32a26bdc11730c2752ec56839e18855cce9d185b
桃花林里练醉拳 <jsmrliu@gmail.com>
2017-02-09 17:09:14 +0800

    rxjava

--- rxjava/src/main/java/com/ls/rxjava/ui/BasicImplementlActivity.java
+++ rxjava/src/main/java/com/ls/rxjava/ui/BasicImplementlActivity.java
@@ -1,6 +1,7 @@
 package com.ls.rxjava.ui;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import com.ls.rxjava.R;
 import com.ls.rxjava.databinding.ActivityBasicImplementBinding;
+
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
@@ -18,7 +20,7 @@ import rx.functions.Action1;
 
 /**
  * RxJava基本用法
- *
+ * <p>
  * 知识点包括：
  * 1> 创建Observer观察者;
  * 2> 创建Subscriber观察者;
@@ -26,12 +28,12 @@ import rx.functions.Action1;
  * 4> 快捷创建被观察者事件队列的just和from方式;
  * 5> 观察者订阅被观察者，两者联系起来;
  * 6> Subscriber不完整定义回调;
- *
+ * <p>
  * 备注：
  * 1> 创建了Observable和Observer之后，再用 subscribe() 方法将它们联结起来，整条链子就可以工作了.
  * 2> 除了subscribe(Observer)和subscribe(Subscriber)，subscribe()还支持观察者的不完整定义的回调，RxJava会自动根据定义创建出Subscriber.
  * 3> Observer和Subscriber都是观察者，而且Observer在subscribe()过程中最终会被转换成Subscriber对象，因此，Subscriber来代替Observer更加严谨.
- *
+ * <p>
  * Created by liusong on 2017/1/31.
  */
 
@@ -70,20 +72,26 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
             case R.id.btn_incomplete_definition:
                 incompleteDefinitionCall();
                 break;
+            case R.id.btn_print_string_array:
+                printStringArray();
+                break;
+            case R.id.btn_show_image:
+                showImageBySubscribeObserver();
+                break;
         }
     }
 
     /**
      * toast提示：无效果
      */
-    private void showNothingMsg(){
+    private void showNothingMsg() {
         Toast.makeText(this, "无效果，主要是创建方法的使用！", Toast.LENGTH_SHORT).show();
     }
 
     /**
      * toast提示：查看log信息
      */
-    private void showLogPrintMsg(){
+    private void showLogPrintMsg() {
         Toast.makeText(this, "查看log打印的信息！", Toast.LENGTH_SHORT).show();
     }
 
@@ -91,7 +99,7 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
      * 创建Observer观察者
      */
     private void createObserver() {
-        Observer<String> observer=new Observer<String>() {
+        Observer<String> observer = new Observer<String>() {
             @Override
             public void onCompleted() {
 
@@ -113,7 +121,7 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
      * 创建Subscriber观察者
      */
     private void createSubscriber() {
-        Subscriber<String> subscriber = new Subscriber<String>(){
+        Subscriber<String> subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
 
@@ -135,7 +143,7 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
      * 创建Subscriber被观察者
      */
     private void createObservable() {
-        Observable observable=Observable.create(new Observable.OnSubscribe<String>(){
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
                 subscriber.onNext("1");
@@ -149,7 +157,7 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
     /**
      * 快捷创建事件队列
      */
-    private void createEventQueueQuickly(){
+    private void createEventQueueQuickly() {
 
         //just(T...): 将传入的参数依次发送出来。
         Observable observable_just = Observable.just("1", "2", "3");
@@ -170,16 +178,16 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
     }
 
     /**
-     * @method_name 订阅
-     * @describe 创建了 Observable 和 Observer 之后，再用 subscribe() 方法将它们联结起来，整条链子就可以工作了.
+     * 订阅：
+     * 创建了 Observable 和 Observer 之后，再用 subscribe() 方法将它们联结起来，整条链子就可以工作了.
      */
     private void subscribe() {
         showLogPrintMsg();
 
-        Observer<String> observer=new Observer<String>() {
+        Observer<String> observer = new Observer<String>() {
             @Override
             public void onCompleted() {
-                Log.i("LOG_CAT","Observer::onCompleted");
+                Log.i("LOG_CAT", "Observer::onCompleted");
             }
 
             @Override
@@ -189,14 +197,14 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
 
             @Override
             public void onNext(String s) {
-                Log.i("LOG_CAT","Observer::onNext("+s+")");
+                Log.i("LOG_CAT", "Observer::onNext(" + s + ")");
             }
         };
 
-        Subscriber<String> subscriber = new Subscriber<String>(){
+        Subscriber<String> subscriber = new Subscriber<String>() {
             @Override
             public void onCompleted() {
-                Log.i("LOG_CAT","Subscriber::onCompleted");
+                Log.i("LOG_CAT", "Subscriber::onCompleted");
             }
 
             @Override
@@ -206,11 +214,11 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
 
             @Override
             public void onNext(String s) {
-                Log.i("LOG_CAT","Subscriber::onNext("+s+")");
+                Log.i("LOG_CAT", "Subscriber::onNext(" + s + ")");
             }
         };
 
-        Observable observable=Observable.create(new Observable.OnSubscribe<String>(){
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
                 subscriber.onNext("1");
@@ -226,17 +234,17 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
     }
 
     /**
-     * @method_name 不完整定义回调
-     * @describe 除了subscribe(Observer)和subscribe(Subscriber)，subscribe()还支持不完整定义的回调，RxJava会自动根据定义创建出Subscriber.
+     * 不完整定义回调：
+     * 除了subscribe(Observer)和subscribe(Subscriber)，subscribe()还支持不完整定义的回调，RxJava会自动根据定义创建出Subscriber.
      */
-    private void incompleteDefinitionCall(){
+    private void incompleteDefinitionCall() {
         showLogPrintMsg();
 
         Action1<String> onNextAction = new Action1<String>() {
             // onNext()
             @Override
             public void call(String s) {
-                Log.i("LOG_CAT", "onNextAction::call("+s+")");
+                Log.i("LOG_CAT", "onNextAction::call(" + s + ")");
             }
         };
         Action1<Throwable> onErrorAction = new Action1<Throwable>() {
@@ -254,7 +262,7 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
             }
         };
 
-        Observable observable=Observable.create(new Observable.OnSubscribe<String>(){
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
             @Override
             public void call(Subscriber<? super String> subscriber) {
                 Log.d("LOG_CAT", "onCompletedAction::call");
@@ -272,4 +280,78 @@ public class BasicImplementlActivity extends AppCompatActivity implements View.O
         // 自动创建 Subscriber ，并使用 onNextAction、 onErrorAction和onCompletedAction来定义onNext()、onError()和onCompleted().
         observable.subscribe(onNextAction, onErrorAction, onCompletedAction);
     }
+
+    /**
+     * 不完整定义回调：事件队列打印字符数组
+     */
+    private void printStringArray() {
+        showLogPrintMsg();
+
+        String[] array = {"welcome", "to", "here", "!"};
+        Observable.from(array)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i("LOG_CAT", s);
+                    }
+                });
+    }
+
+    /**
+     * 订阅Observer:由id取得Drawable图片并显示在ImageView中
+     */
+    private void showImageBySubscribeObserver(){
+
+        Observable.create(new Observable.OnSubscribe<Drawable>() {
+            @Override
+            public void call(Subscriber<? super Drawable> subscriber) {
+                Drawable drawable=getTheme().getDrawable(R.mipmap.uselessl);
+                subscriber.onNext(drawable);
+                subscriber.onCompleted();
+            }
+        }).subscribe(new Observer<Drawable>() {
+            @Override
+            public void onCompleted() {
+                Log.i("LOG_CAT", "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i("LOG_CAT", "onError");
+                Toast.makeText(BasicImplementlActivity.this, "display image error!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(Drawable drawable) {
+                Log.i("LOG_CAT", "onNext");
+                mBinding.ivImage.setImageDrawable(drawable);
+            }
+        });
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
