25cc7e701b3c02a01dcef58cc2814080b074ed95
桃花林里练醉拳 <jsmrliu@gmail.com>
2017-02-15 17:55:57 +0800

    retrofit 2.1.0

--- retrofit/src/main/java/com/ls/retrofit/MainActivity.java
+++ retrofit/src/main/java/com/ls/retrofit/MainActivity.java
@@ -1,13 +1,18 @@
 package com.ls.retrofit;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ls.retrofit.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
