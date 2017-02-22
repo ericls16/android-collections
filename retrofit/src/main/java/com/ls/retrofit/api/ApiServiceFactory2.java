a693d27639c65732344ba6c9c044219f15cbf709
桃花林里练醉拳 <jsmrliu@gmail.com>
2017-02-22 11:33:25 +0800

    retrofit

--- retrofit/src/main/java/com/ls/retrofit/api/ApiServiceFactory2.java
+++ retrofit/src/main/java/com/ls/retrofit/api/ApiServiceFactory2.java
@@ -1,7 +1,7 @@
 package com.ls.retrofit.api;
 
 import android.support.annotation.NonNull;
-import android.support.v4.util.ArrayMap;
+import android.util.ArrayMap;
 
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.ls.retrofit.app.App;
@@ -16,42 +16,21 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Created by liu song on 2017/2/21.
+ * ApiServiceFactory2
+ *
+ * 本类采用静态代码块来初始化retrofit
+ * Created by liu song on 2017/2/22.
  */
 
-public class ApiServiceFactory {
+public class ApiServiceFactory2 {
 
+    private static final Retrofit RETROFIT;
     private static final Map<Class<?>, Object> SERVICE_MAP = new ArrayMap<>();
     //volatile关键字禁止JVM指令重排序优化
-    private volatile static ApiServiceFactory INSTANCE;
-    private static Retrofit RETROFIT;
+    private volatile static ApiServiceFactory2 INSTANCE;
 
-    private ApiServiceFactory() {
-        initRetrofit();
-    }
-
-    public static ApiServiceFactory getInstance() {
-        if (INSTANCE == null) {
-            synchronized (ApiServiceFactory.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = new ApiServiceFactory();
-                }
-            }
-        }
-        return INSTANCE;
-    }
-
-    /**
-     * 初始化Retrofit：
-     * -------------------
-     * okhttpClient相关设置：
-     * 超时设置：
-     * connectTimeout(DEFAULT_CONNECT_TIMEOUT, TimeUnit.SECONDS)
-     * readTimeout(DEFAULT_READ_TIMEOUT, TimeUnit.SECONDS)
-     * writeTimeout(DEFAULT_WRITE_TIMEOUT, TimeUnit.SECONDS)
-     * -------------------
-     */
-    private static void initRetrofit() {
+    static {
+        //静态代码块只有在类被第一次加载的时候执行一次，new实例也不会再次执行。
         OkHttpClient okhttpClient = new OkHttpClient.Builder()
                 .addNetworkInterceptor(new StethoInterceptor())
                 .cookieJar(new CookieManager(App.getInstance())) //自动管理cookie
@@ -64,7 +43,18 @@ public class ApiServiceFactory {
                 .build();
     }
 
-    public <T> T create(@NonNull Class<T> service) {
+    public static ApiServiceFactory2 getInstance() {
+        if (INSTANCE == null) {
+            synchronized (ApiServiceFactory2.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ApiServiceFactory2();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public static <T> T create(@NonNull Class<T> service) {
         Object o = SERVICE_MAP.get(service);
         if (o != null) {
             return (T) o;
@@ -74,5 +64,4 @@ public class ApiServiceFactory {
             return t;
         }
     }
-
 }
